a58514d4fa9e232ce9de258dc4f80dd378b80240
Adrian Craciun <adicraciun@Adrians-MacBook-Pro.local>
2017-03-25 16:43:44 +0200

    Minor fixes

--- car_controller.cpp
+++ car_controller.cpp
@@ -1,16 +1,20 @@
 #include "car_controller.h"
-#include "car_state.h"
-#include "car_connection.h"
 
 CarController::CarController() {}
 
 void CarController::Start() {
-  CarState* state = new CarState();
+  state = new CarState();
   std::string ip = "0.0.0.0";
   int port = 5000;
 
-  CarConnection* connection = new CarConnection(state, make_pair(ip, port));
+  connection = new CarConnection(state, make_pair(ip, port));
 
   connection->Connect();
   connection->Start();
+}
+
+
+CarController::~CarController() {
+  delete state;
+  delete connection;
 }
--- car_controller.h
+++ car_controller.h
@@ -1,9 +1,16 @@
 #ifndef _CAR_CONTROLLER_H_
 #define _CAR_CONTROLLER_H_
 
+#include "car_state.h"
+#include "car_connection.h"
+
 class CarController {
+  private:
+    CarConnection* connection; // OWNER
+    CarState* state; // OWNER
   public:
     CarController();
+    ~CarController();
     void Start();
   private:
 
