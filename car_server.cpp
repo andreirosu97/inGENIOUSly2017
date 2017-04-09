7eb323e0da3d07c9c4f3a667a8fd2e5a742392ef
adicraciun <adi.craciun96@gmail.com>
2017-04-09 15:17:33 +0300

    Removed rendundant files

--- car_server.cpp
+++ car_server.cpp
@@ -19,15 +19,11 @@ std::string CarServer::GetIPAddress() {
       exit(EXIT_FAILURE);
   }
 
-  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
-  {
+  for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
       if (ifa->ifa_addr == NULL)
           continue;
-
       s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-
-      if((strcmp(ifa->ifa_name, "wlan0")==0) && (ifa->ifa_addr->sa_family==AF_INET))
-      {
+      if((strcmp(ifa->ifa_name, "wlan0")==0) && (ifa->ifa_addr->sa_family==AF_INET)) {
           if (s != 0) {
               printf("getnameinfo() failed: %s\n", gai_strerror(s));
           }
