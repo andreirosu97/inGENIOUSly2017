e63ac4596de87168e84afa7a1564359e4c6e3b7e
Andrei <andreirosu97@gmail.com>
2017-04-12 23:12:07 +0300

    Time measure

--- car_ultrasonic.cpp
+++ car_ultrasonic.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <chrono>
+#include <ctime>
+#include <unistd.h>
 using namespace std;
 
 int main()
 {
-    auto x=1;
-    auto c='a';
-    std::cout<<x<<c;
-    auto begin = std::chrono::high_resolution_clock::now();
-
-    for(int i = 0; i < 35000; ++i);
-        //std::cout<<"cacat";
-
-    auto end = std::chrono::high_resolution_clock::now();
-
-    std::cout << std::chrono::duration_cast<std::chrono::seconds>(end-begin).count() << "ns" << std::endl;
+    clock_t cl;
+    cl = clock();
+    sleep(5);
+    cl = clock() - cl;
+    cout <<(float)cl/10 << endl;
     return 0;
 }
