0ac9e202fbba01c1eb9bedf566494164fa77e350
Andrei <andreirosu97@gmail.com>
2017-05-01 14:25:50 +0300

    4 Motors

--- car_motor.h
+++ car_motor.h
@@ -4,9 +4,21 @@
 #include "car_state.h"
 #include <thread>
 
-#define PWM_PIN 0
-#define PIN_1 1
-#define PIN_2 2
+#define PWM_1 0
+#define PIN_1_1 1
+#define PIN_1_2 2
+
+#define PWM_2 3
+#define PIN_2_1 4
+#define PIN_2_2 5
+
+#define PWM_3 8
+#define PIN_3_1 9
+#define PIN_3_2 7
+
+#define PWM_4 6
+#define PIN_4_1 10
+#define PIN_4_2 11
 
 class CarMotor {
 private:
